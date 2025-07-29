d1cc4959bd49be1bbece40db8ffa6182860e2754
Dennis <kwesimodestygh111@gmail.com>
2025-07-28 22:22:55 -0800

    base url added to vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: VITE_API_BASE_URL,
         changeOrigin: true,
         secure: false,
       }
