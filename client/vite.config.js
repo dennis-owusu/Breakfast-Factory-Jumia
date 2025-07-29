acc5a3ecc8a748e26a24607fed50b5cd3add86c0
Dennis <kwesimodestygh111@gmail.com>
2025-07-29 09:22:34 -0800

    changes url

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://breakfast-factory-jumia.onrender.com',
         secure: false,
       }
     }
